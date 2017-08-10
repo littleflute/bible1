6bd79519543886cfeeff3afe6ce15affa135f29b
littleflute <13699175164@sina.cn>
2017-08-10 13:43:15 -0500

    Add files via upload

---
+++ NIV Live A Bible Experience/OldTestament/cd02/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Genesis 18.mp3";		
+f[2]	= "02 Genesis 19.mp3";		
+f[3]	= "03 Genesis 20.mp3";		
+f[4]	= "04 Genesis 21.mp3";		
+f[5]	= "05 Genesis 22.mp3";		
+f[6]	= "06 Genesis 23.mp3";		
+f[7]	= "07 Genesis 24.mp3";		
+f[8]	= "08 Genesis 25.mp3";		
+f[9]	= "09 Genesis 26.mp3";		
+f[10]	= "10 Genesis 27.mp3";		
+f[11]	= "11 Genesis 28.mp3";		
+f[12]	= "12 Genesis 29.mp3";		
